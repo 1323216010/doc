21253b3785685358d4b25d11dbfd88323591663a
13232160100 <ypc0100@qq.com>
2023-03-31 16:57:27 +0800

    测试

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,9 +1,5 @@
 const ogprefix = 'og: http://ogp.me/ns#'
 module.exports = {
-  extendMarkdown(md) {
-    md.set({ html: true });
-    md.use(require("markdown-it-katex"));
-  },
   title: '幻想乡 v1.0',
   description: 'Open source Instant Search Engine',
   theme: 'default-prefers-color-scheme',
@@ -166,7 +162,7 @@ module.exports = {
   ],
   head: [
     ['link', { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.7.1/katex.min.css' }],
-    ['link', { rel: "stylesheet", href: "https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.10.0/github-markdown.min.css" }],
+    ['link', { rel: "stylesheet", href: 'https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.10.0/github-markdown.min.css' }],
     ['link', { rel: 'icon', href: '/favicon-32x32.png' }],
     ['link', { rel: 'preconnect', href: 'https://fonts.googleapis.com' }],
     ['link', { rel: 'preconnect', href: 'https://fonts.gstatic.com' }],
--- .vuepress/custom-markdown-rules.js
+++ .vuepress/custom-markdown-rules.js
@@ -4,4 +4,8 @@ module.exports = (options, context) => ({
       .linkify(true) // Autoconvert URL-like text to links
       .breaks(true) // Convert '\n' in paragraphs into <br>
   },
+  extendMarkdown(md) {
+    md.set({ html: true });
+    md.use(require("markdown-it-katex"));
+  },
 })
